5fe768af7c72cd1d93eb629dcfbd6320e8a30d82
emmanuel-jan <50714005+emmanuel-jan@users.noreply.github.com>
2020-05-07 20:51:05 +0300

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,4 +1,4 @@
 <?php
 //establishing a connection to the database
-$conn=mysqli_connect("localhost","root","Emaochi99","phpcms"); 
+$conn=mysqli_connect("us-cdbr-east-06.cleardb.net","bbc254d8bb6a7d","2540353d","heroku_cec9f6d0f0bc5ac"); 
 ?>
